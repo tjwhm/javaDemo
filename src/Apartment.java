d5d5ba1945654247780e79102fd52291b271ea30
tjwhm <759361109@qq.com>
2017-10-08 17:39:23 +0800

    added innerclass

--- src/Apartment.java
+++ src/Apartment.java
@@ -21,4 +21,45 @@ public class Apartment {
     public static int getTotalViewer() {
         return totalViewer;
     }
+
+    class Location {
+        City city;
+        String street;
+        int number;
+        int floor;
+        boolean faceEast;
+
+        public Location(City city, String street, int number, int floor, boolean faceEast) {
+            this.city = city;
+            this.street = street;
+            this.number = number;
+            this.floor = floor;
+            this.faceEast = faceEast;
+        }
+
+        public City getCity() {
+            ++totalViewer;
+            return city;
+        }
+
+        public String getStreet() {
+            ++totalViewer;
+            return street;
+        }
+
+        public int getNumber() {
+            ++totalViewer;
+            return number;
+        }
+
+        public int getFloor() {
+            ++totalViewer;
+            return floor;
+        }
+
+        public boolean isFaceEast() {
+            ++totalViewer;
+            return faceEast;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Apartment a1 = new Apartment(200);
-        a1.getSize();
-        a1.getSize();
-
-        System.out.println(a1.totalViewer);
-        System.out.println(Apartment.totalViewer);
-        System.out.println(a1.getTotalViewer());
-        System.out.println(Apartment.getTotalViewer());
-
-        Apartment a2 = new Apartment(300);
-        a2.getSize();
-        a2.getSize();
-        System.out.println(a2.totalViewer);
-        System.out.println(Apartment.totalViewer);
-        System.out.println(a2.getTotalViewer());
-        System.out.println(Apartment.getTotalViewer());
+        Apartment apartment = new Apartment(300);
+        Apartment.Location location = apartment.new Location(City.BEIJING,"Chang'An",305,3,true);
     }
 }
